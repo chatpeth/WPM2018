14914733152dc81ddde44f9b33a9337ef96a3704
CHATPETH KENANAN <5610110106@email.psu.ac.th>
2018-05-23 09:57:14 +0700

    release05232018

--- NodeMCU_Source_File/wpm2018_release05052018/wpm2018_release05222018/wpm2018_release05222018.ino
+++ NodeMCU_Source_File/wpm2018_release05052018/wpm2018_release05222018/wpm2018_release05222018.ino
@@ -12,7 +12,7 @@
 //#define SIM_MODE        // Define, If need to simulation pulse in.
 #define ON 1
 #define OFF 0
-#define NID "10"
+#define NID "5"
 #define SW "sw" NID
 #define ALM "Alarm" NID
 #define T_MAX 1000000
@@ -79,8 +79,8 @@ structPhase phaseID[3] = { {1, 14, 0, 0}, {1, 12, 0, 0}, {1, 13, 0, 0} };
 int sw_status = ON;
 int count_connect = 0;
 char nodeID[5] = NID;
-const char* ssid = "atop802.11x";
-const char* password = "atop3352";
+const char* ssid = "PowerMeterIoT_WiFi";
+const char* password = "password";
 IPAddress ip(192, 168, 0, 140); //set static ip
 IPAddress gateway(192, 168, 0, 100); //set getteway
 IPAddress subnet(255, 255, 255, 0);//set subnet
